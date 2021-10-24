f3f71be897692b42fe60a155a7a0c5b35a096715
yjlee90 <olrai011@gmail.com>
2021-10-24 14:41:24 +0900

    blacked

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 def helloworld(a):
     msg = f"Hello World! {a}!"
-    print(msg)  
+    print(msg)
     return msg
 
+
 def helloworld2(a):
-    print(f"Hello World2! {a}!")  
+    print(f"Hello World2! {a}!")
+
 
 if __name__ == "__main__":
     helloworld("Yoojong")
-
-
-
-
-
--- main_test.py
+++ main_test.py
@@ -2,12 +2,11 @@ import unittest
 import main
 
 
-class MainTest(unittest.TestCase) :
-    def test_helloworld(self)  :
-        ret = main.helloworld("Test") 
+class MainTest(unittest.TestCase):
+    def test_helloworld(self):
+        ret = main.helloworld("Test")
         self.assertEqual(ret, "Hello World! Test!")
-    
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     unittest.main()
